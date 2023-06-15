2dfe6ca8a4febbfeda0d877b9c029f02d7235d41
sagid.veliev <dagfarid73@gmail.com>
2023-06-15 20:27:42 +0400

    sixth commit

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,8 @@
 import { Node } from '@/models/types';
 import GraphNode from '@/utils/graphNode';
+import GraphBranch from '@/utils/graphBranch';
 import clear from '@/utils/clearPlayground';
 // import GraphArrow from '@/utils/graphArrow';
-// import GraphNode from '@/utils/graphNode';
-// import GraphBranch from '@/utils/graphBranch';
 
 // let prevCalcTop = 0;
 // let prevBranchInstance: any;
@@ -83,8 +82,8 @@ export default function gitNode(command: string, nodes: Node[], id: number): Nod
         const mergedBranch = getMergeName(mergeCommand);
         // eslint-disable-next-line no-unused-vars
         let mergedId = 0;
-        searchLatestCommitOfMergedBeanch(nodes);
-        function searchLatestCommitOfMergedBeanch(array: Node[]) {
+        (function searchLatestCommitOfMergedBeanch(array: Node[]) {
+            debugger;
             array.forEach((item: Node) => {
                 if (item.currentBranch === mergedBranch) {
                     if (item.children.length === 0) {
@@ -102,18 +101,16 @@ export default function gitNode(command: string, nodes: Node[], id: number): Nod
                     }
                 }
             });
-        }
-        recursiveMergedBranch(mergedId);
-        function recursiveMergedBranch(merged: number) {
-            debugger;
-            nodes.forEach((node: Node) => {
+        }(nodes));
+        (function recursiveMergedBranch(array: Node[]) {
+            array.forEach((node: Node) => {
                 if (node.currentNode) {
-                    node.parent.push(merged);
+                    node.parent.push(mergedId);
                 } else {
-                    recursiveMergedBranch(mergedId);
+                    recursiveMergedBranch(node.children);
                 }
             });
-        }
+        }(nodes));
     }
     const node: Node = {
         id,
@@ -173,17 +170,28 @@ export default function gitNode(command: string, nodes: Node[], id: number): Nod
     return nodes;
 }
 
-interface GitNode { node: Node; positionY: number; positionX: number; }
+interface GitNode { node: Node; positionY: number; positionX: number; positionXBranch: number; }
 
 function createNodes(nodes: Node[]) {
     let calcTop = 10;
     let calcLeft = 45;
+    let calcLeftBranch = 49;
     (function recursive(array: Node[]) {
         const queue: GitNode[] = [];
         const result: GitNode[] = [];
         array.forEach((commit: Node) => {
-            let current: GitNode | undefined = { node: commit, positionY: calcTop, positionX: calcLeft };
-            queue.push({ node: commit, positionY: calcTop, positionX: calcLeft });
+            let current: GitNode | undefined = {
+                node: commit,
+                positionY: calcTop,
+                positionX: calcLeft,
+                positionXBranch: calcLeftBranch,
+            };
+            queue.push({
+                node: commit,
+                positionY: calcTop,
+                positionX: calcLeft,
+                positionXBranch: calcLeftBranch,
+            });
             while (queue.length) {
                 current = queue.shift();
                 if (current) {
@@ -193,6 +201,7 @@ function createNodes(nodes: Node[]) {
                 if (children) {
                     if (children > 1) {
                         calcLeft = 90 / (children + 1);
+                        calcLeftBranch = (90 / (children + 1)) + 1;
                     } else {
                         // eslint-disable-next-line no-loop-func
                         const parentNode = result.find((parent: GitNode) => parent.node.id === current?.node.children[0].parent[0]);
@@ -201,13 +210,17 @@ function createNodes(nodes: Node[]) {
                     calcTop += 90;
                     // eslint-disable-next-line no-loop-func
                     current?.node.children.forEach((child: Node, index: number) => {
-                        queue.push({ node: child, positionY: calcTop, positionX: calcLeft * (index + 1) });
+                        queue.push({
+                            node: child,
+                            positionY: calcTop,
+                            positionX: calcLeft * (index + 1),
+                            positionXBranch: calcLeftBranch * (index + 1),
+                        });
                     });
                     calcLeft = 45;
                 }
             }
         });
-        console.log(result);
         createGraph(result);
     }(nodes));
 }
@@ -217,5 +230,7 @@ function createGraph(nodes: GitNode[]) {
     nodes.forEach((node: GitNode) => {
         const nodeInstanceChildren = new GraphNode(node.node.name, node.positionY, node.positionX);
         nodeInstanceChildren.createNode();
+        const branchInstanceChildren = new GraphBranch(node.node.branch, currBranch, node.positionY, node.positionXBranch);
+        branchInstanceChildren.createBranch();
     });
 }
